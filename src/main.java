5a561ff13cbf1462386ffd986615ad8a3d75ec71
= <=>
2024-03-05 16:47:42 +0800

    student

---
+++ src/main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;  
+  
+public class main {  
+    public static void main(String[] args) {  
+        Scanner scanner = new Scanner(System.in);  
+        student student = null;  
+  
+        while (true) {  
+            System.out.print("Please enter a student ID:(or give quit to stop it): ");  
+            String inputId = scanner.nextLine();  
+            if (inputId.equalsIgnoreCase("quit")) {  
+                break;  
+            }  
+  
+            System.out.print("Please enter a student name: ");  
+            String name = scanner.nextLine();  
+  
+            System.out.print("Please enter the number of the extra activities: ");  
+            int numActivities = scanner.nextInt();  
+            scanner.nextLine(); // 消耗nextInt留下的换行符  
+  
+            student = new student(inputId, name, numActivities);  
+  
+            for (int i = 0; i < numActivities; i++) {  
+                System.out.print("Please give the " + (i + 1) + " extra activities: ");  
+                String activity = scanner.nextLine();  
+                student.addActivity(i, activity);  
+            }  
+  
+            System.out.println(student); // 打印学生信息  
+        }  
+  
+        scanner.close();  
+    }  
+}
